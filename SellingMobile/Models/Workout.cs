7a33d036e65bef4a68700ce764684ae9b303fe2e
DatDieu <dat.dieu@tpssoft.com>
2018-11-30 16:19:18 +0700

    Commit Selling Mobile

---
+++ SellingMobile/Models/Workout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SellingMobile.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset Date { get; set; }
+
+        public int DistanceInMeters { get; set; }
+
+        public double TimeInSeconds { get; set; }
+    }
+}
